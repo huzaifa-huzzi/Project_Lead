80a5389270909a93551465d1e42bd32e2b8200a3
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-07-10 13:49:20 +0500

    Making New Screen and setting the Routes

--- lib/Routes/Routes_initiliazing.dart
+++ lib/Routes/Routes_initiliazing.dart
@@ -2,12 +2,20 @@ import 'package:get/get.dart';
 import 'package:project_x/Routes/Routes_name.dart';
 import 'package:project_x/View/Authentication/Login/LoginScreen.dart';
 import 'package:project_x/View/Dashboard/DashboardScreen.dart';
+import 'package:project_x/View/Form/Form.dart';
+import 'package:project_x/View/Home/HomeScreen.dart';
+import 'package:project_x/View/Verify/VerifyScreen.dart';
 
 class AppRoutes {
   
   static appRoutes () => [
+    /// Authentication
     GetPage(name:RouteName.loginScreen , page: () => const LoginScreen()),
+    /// Screens
     GetPage(name:RouteName.dashboardScreen , page: () =>  DashboardScreen()),
+    GetPage(name:RouteName.homeScreen , page: () =>  HomeScreen()),
+    GetPage(name:RouteName.formsFillingScreen , page: () =>  FormFilling()),
+    GetPage(name:RouteName.verifyScreen , page: () =>  VerifyScreen()),
   ];
   
   
--- lib/Routes/Routes_name.dart
+++ lib/Routes/Routes_name.dart
@@ -7,5 +7,8 @@ class RouteName {
   static const String loginScreen = '/LoginScreen';
   // view
   static const String dashboardScreen = '/DashboardScreen';
+  static const String homeScreen = '/HomeScreen';
+  static const String formsFillingScreen = '/FormFilling';
+  static const String verifyScreen = '/VerifyScreen';
 
 }

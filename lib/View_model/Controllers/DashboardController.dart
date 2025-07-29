188761c362b9e79b8046b1b64602ac04bc72e7f3
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-07-29 19:56:35 +0500

    Setting of my code

--- lib/View_model/Controllers/DashboardController.dart
+++ lib/View_model/Controllers/DashboardController.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 class DashboardController extends GetxController {
   var selectedIndex = 0.obs;
 
+   /// Logic for the change of the screen
   void changeScreen(int index) {
     selectedIndex.value = index;
   }
